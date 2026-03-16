fd91a8c3beae59446fd5b0cceb5c8112b7963739
XueshiQiao <622533+XueshiQiao@users.noreply.github.com>
2026-03-16 02:38:38 +0000

    Update cask for pastepaw

--- Casks/pastepaw.rb
+++ Casks/pastepaw.rb
@@ -1,7 +1,7 @@
 cask "pastepaw" do
-  version "1.2.3"
+  version "1.2.5"
   sha256 arm:   "496ec3ef0b6606555693d63debf3814d065e7da38a5ff0eecc50f8e23f3e240c",
-         intel: "3c01b62841fbea02d3f2290db5ad48e962805666930990a3d0a90a99fec49814"
+         intel: "54ac8d922086f9919a2b39da632d9cc0eb5972631c36a8c88cf9d1d8bd6f8c46"
 
   on_arm do
     url "https://github.com/XueshiQiao/PastePaw/releases/download/main/PastePaw_aarch64.app.tar.gz"
