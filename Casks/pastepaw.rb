bd1ada94373e93e9e5d643336419d449235b402c
XueshiQiao <622533+XueshiQiao@users.noreply.github.com>
2026-03-16 02:40:08 +0000

    Update cask for pastepaw

--- Casks/pastepaw.rb
+++ Casks/pastepaw.rb
@@ -1,6 +1,6 @@
 cask "pastepaw" do
   version "1.2.5"
-  sha256 arm:   "496ec3ef0b6606555693d63debf3814d065e7da38a5ff0eecc50f8e23f3e240c",
+  sha256 arm:   "33f4ef55fbfac3ae334ee13a262ae23160b71410c0c416b25c9bc0d737c8d58d",
          intel: "54ac8d922086f9919a2b39da632d9cc0eb5972631c36a8c88cf9d1d8bd6f8c46"
 
   on_arm do
