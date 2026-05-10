94bb9b5d50645d0444bfa75d5bfe6a2014cfc16e
XueshiQiao <622533+XueshiQiao@users.noreply.github.com>
2026-05-10 10:53:57 +0000

    Update cask for ccswitcher

--- Casks/ccswitcher.rb
+++ Casks/ccswitcher.rb
@@ -1,13 +1,13 @@
 cask "ccswitcher" do
-  version "1.5.7"
-  sha256 arm:   "50d1a87ef2ab1c1a6ed486f19bf715d059c108343ba0c44009bbeca321985c5d",
-         intel: "50d1a87ef2ab1c1a6ed486f19bf715d059c108343ba0c44009bbeca321985c5d"
+  version "1.5.8"
+  sha256 arm:   "400b3c608639d38f2f076849e7c6d4baffc122381e5c3ff3c550a16dc00fa7a9",
+         intel: "400b3c608639d38f2f076849e7c6d4baffc122381e5c3ff3c550a16dc00fa7a9"
 
   on_arm do
-    url "https://github.com/XueshiQiao/CCSwitcher/releases/download/v1.5.7/CCSwitcher.dmg"
+    url "https://github.com/XueshiQiao/CCSwitcher/releases/download/v1.5.8/CCSwitcher.dmg"
   end
   on_intel do
-    url "https://github.com/XueshiQiao/CCSwitcher/releases/download/v1.5.7/CCSwitcher.dmg"
+    url "https://github.com/XueshiQiao/CCSwitcher/releases/download/v1.5.8/CCSwitcher.dmg"
   end
 
   name "CCSwitcher"
