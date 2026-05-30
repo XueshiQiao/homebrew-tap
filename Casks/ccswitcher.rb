37b4d83aca4849f628da354b274ee57f8c1f8be2
XueshiQiao <622533+XueshiQiao@users.noreply.github.com>
2026-05-30 04:39:35 +0000

    Update cask for ccswitcher

--- Casks/ccswitcher.rb
+++ Casks/ccswitcher.rb
@@ -1,25 +1,25 @@
 cask "ccswitcher" do
-  version "1.8.3"
-  sha256 arm:   "ef26f43bd6cdc262b6307bf0daa62f266c7c86de5b51d59da9d49768b29da2d1",
-         intel: "ef26f43bd6cdc262b6307bf0daa62f266c7c86de5b51d59da9d49768b29da2d1"
+  version "1.9.0"
+  sha256 arm:   "5fff9db856fbf64ffab6adbf1c63592499c0425606d6324f6dff6886e30405d9",
+         intel: "5fff9db856fbf64ffab6adbf1c63592499c0425606d6324f6dff6886e30405d9"
 
   on_arm do
-    url "https://github.com/XueshiQiao/CCSwitcher/releases/download/v1.8.3/CCSwitcher.dmg"
+    url "https://github.com/XueshiQiao/CCSwitcher/releases/download/v1.9.0/CCSwitcher.dmg"
   end
   on_intel do
-    url "https://github.com/XueshiQiao/CCSwitcher/releases/download/v1.8.3/CCSwitcher.dmg"
+    url "https://github.com/XueshiQiao/CCSwitcher/releases/download/v1.9.0/CCSwitcher.dmg"
   end
 
   name "CCSwitcher"
   desc "Menu bar app to manage and switch between Claude Code accounts"
   homepage "https://github.com/XueshiQiao/CCSwitcher"
 
-  auto_updates true
-
   livecheck do
     url "https://github.com/XueshiQiao/CCSwitcher/releases/latest/download/latest.json"
     regex(/"version"\s*:\s*"([^"]+)"/i)
   end
+
+  auto_updates true
 
   app "CCSwitcher.app"
 end
