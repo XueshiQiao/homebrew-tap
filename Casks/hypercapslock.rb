8fb45ca8cb54cb12b50b8d0a7c4932e9984fb080
XueshiQiao <622533+XueshiQiao@users.noreply.github.com>
2026-06-10 11:48:38 +0000

    Update cask for hypercapslock

--- Casks/hypercapslock.rb
+++ Casks/hypercapslock.rb
@@ -1,13 +1,13 @@
 cask "hypercapslock" do
-  version "26.06.103"
-  sha256 arm:   "53bd6385a8a1ae1c6ae6219051f6fb7e6559cacad9cb422aa65bea46e824c848",
-         intel: "53bd6385a8a1ae1c6ae6219051f6fb7e6559cacad9cb422aa65bea46e824c848"
+  version "26.06.104"
+  sha256 arm:   "b3e1dcf2492f480d15877aa212c2854cbfc94c4e456bba16b531c99440638c6b",
+         intel: "b3e1dcf2492f480d15877aa212c2854cbfc94c4e456bba16b531c99440638c6b"
 
   on_arm do
-    url "https://github.com/XueshiQiao/HyperCapslock/releases/download/v26.06.103/HyperCapslock.dmg"
+    url "https://github.com/XueshiQiao/HyperCapslock/releases/download/v26.06.104/HyperCapslock.dmg"
   end
   on_intel do
-    url "https://github.com/XueshiQiao/HyperCapslock/releases/download/v26.06.103/HyperCapslock.dmg"
+    url "https://github.com/XueshiQiao/HyperCapslock/releases/download/v26.06.104/HyperCapslock.dmg"
   end
 
   name "HyperCapslock"
