718a1df713d0deab1c0e8be56f48238ba6ad7eea
XueshiQiao <622533+XueshiQiao@users.noreply.github.com>
2026-09-17 12:18:27 +0000

    Update cask for anydrag

--- Casks/anydrag.rb
+++ Casks/anydrag.rb
@@ -1,13 +1,13 @@
 cask "anydrag" do
-  version "26.09.114"
-  sha256 arm:   "60e0408ef1ae68b7ce10e09b83c6ed6529759d1bb3b9c6c8c019fa84f254b6cd",
-         intel: "60e0408ef1ae68b7ce10e09b83c6ed6529759d1bb3b9c6c8c019fa84f254b6cd"
+  version "26.09.115"
+  sha256 arm:   "90371a78956100375ff8bb23af1b873d10cbf8ee031fb6e09ec25da4d0bd83a4",
+         intel: "90371a78956100375ff8bb23af1b873d10cbf8ee031fb6e09ec25da4d0bd83a4"
 
   on_arm do
-    url "https://github.com/XueshiQiao/AnyDrag/releases/download/v26.09.114/AnyDrag.dmg"
+    url "https://github.com/XueshiQiao/AnyDrag/releases/download/v26.09.115/AnyDrag.dmg"
   end
   on_intel do
-    url "https://github.com/XueshiQiao/AnyDrag/releases/download/v26.09.114/AnyDrag.dmg"
+    url "https://github.com/XueshiQiao/AnyDrag/releases/download/v26.09.115/AnyDrag.dmg"
   end
 
   name "AnyDrag"
